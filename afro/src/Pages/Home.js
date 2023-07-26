ac55250394b17f683a532009c2c45d30925c17ef
Inayat-khan <khanetmoi@gmail.com>
2023-07-26 12:32:16 +0000

    home Page

--- afro/src/Pages/Home.js
+++ afro/src/Pages/Home.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import comingTo from '../Images/comingTo.jpg'
+import disney from '../Images/disney.jpg'
+import logo from '../Images/logo.png'
+import rwanda from '../Images/rwanda.jpg'
 
 
 const Navigation = styled.div`
 display: flex;
 justify-content: space-between;
 align-items: center;
+background-color: rgba(139, 69, 19, 255);
+color: white;
+font-size: 20px;
    ul {
     display: flex;
     justify-content: space-between;
@@ -18,6 +25,66 @@ align-items: center;
    }
 `;
 
+const Sponsored = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: no-wrap;
+overflow-x: auto;
+width: 90%;
+margin: 0 5%;
+background-color: rgba(139, 69, 19, 255);
+
+    .Scard {
+        flex: 0 0 18%;
+        margin: 0 1%;
+      
+     div {
+         position:relative;
+        img {
+            width: 100%;
+        }
+
+        button {
+          position: absolute;
+          top: 30%;
+          left: 30%;
+          width: 40%;
+          aspect-ratio: 1;
+          background-color: rgba(0,0,0, 0.5);
+          color: white;
+        }
+     }
+   }
+`;
+
+
+const MovieList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 90%;
+  margin: 0 5%;
+  background-color: rgba(139, 69, 19, 255);
+  padding: 20px 0;
+   .MovieCard {
+    width: 18%;
+    margin: 0 1%;
+  
+ div {
+     position:relative;
+    img {
+        width: 100%;
+    }
+
+    button {
+      position: absolute;
+      top: 30%;
+      left: 30%;
+      width: 40%;
+      aspect-ratio: 1;
+      background-color: rgba(0,0,0, 0.5);
+      color: white;
+    }
+`;
 const Home = ()=>{
     const sortByLikes = ()=>{ console.log("sortBylikes")}
     const sortByName = ()=>{console.log("sortByName")}
@@ -27,7 +94,7 @@ const Home = ()=>{
     return (
         <>
         <Navigation>
-        <img src="logo.png" alt="logo.png"/>
+        <img src={logo} alt="logo.png"/>
         <ul>
         <li onClick={sortByLikes}>Home</li>
           <li onclick={sortByName}>Movie List</li>
@@ -44,26 +111,210 @@ const Home = ()=>{
         </div>
         </Navigation>
         {/* Sponsored */}
-        <div className="Sponsored">
+        <h2 className="title">Sponsored</h2>
+        <Sponsored>
+          <div className='Scard'>
           <div>
-            <img src="poster.mp4" alt="poster.mp4"/>
+            <img src={comingTo} alt="poster.mp4"/>
             <button>Play</button>
           </div>
 
           <h2>expendable</h2>
           <h3>2022 . 60 mn</h3>
-        </div>
-        <div className="MovieList">
+          </div>
+          <div className='Scard'>
+          <div>
+            <img src={comingTo} alt="poster.mp4"/>
+            <button>Play</button>
+          </div>
+
+          <h2>expendable</h2>
+          <h3>2022 . 60 mn</h3>
+          </div>
+          <div className='Scard'>
+          <div>
+            <img src={comingTo} alt="poster.mp4"/>
+            <button>Play</button>
+          </div>
+
+          <h2>expendable</h2>
+          <h3>2022 . 60 mn</h3>
+          </div>
+          <div className='Scard'>
+          <div>
+            <img src={comingTo} alt="poster.mp4"/>
+            <button>Play</button>
+          </div>
+
+          <h2>expendable</h2>
+          <h3>2022 . 60 mn</h3>
+          </div>
+          <div className='Scard'>
+          <div>
+            <img src={comingTo} alt="poster.mp4"/>
+            <button>Play</button>
+          </div>
+
+          <h2>expendable</h2>
+          <h3>2022 . 60 mn</h3>
+          </div>
+          <div className='Scard'>
+          <div>
+            <img src={comingTo} alt="poster.mp4"/>
+            <button>Play</button>
+          </div>
+
+          <h2>expendable</h2>
+          <h3>2022 . 60 mn</h3>
+          </div>
+          <div className='Scard'>
+          <div>
+            <img src={comingTo} alt="poster.mp4"/>
+            <button>Play</button>
+          </div>
+
+          <h2>expendable</h2>
+          <h3>2023 . 60 mn</h3>
+          </div>
+        </Sponsored>
+        <h2 className="title">Movie List </h2>
+        <MovieList>
             <div className="MovieCard">
                <div>
-                 <img src="luchano.jpeg" alt="picture.mp4"/>
+                 <img src={disney} alt="picture.mp4"/>
                  <button>Play</button>
                 </div>
 
                  <h2>Expendable</h2>
                 <h3>2022 . 60 mn</h3>
             </div>
-        </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+            <div className="MovieCard">
+               <div>
+                 <img src={disney} alt="picture.mp4"/>
+                 <button>Play</button>
+                </div>
+
+                 <h2>Expendable</h2>
+                <h3>2022 . 60 mn</h3>
+            </div>
+        </MovieList>
         <footer>
             <ul>
                 <li>Questions? Contact us.</li>
