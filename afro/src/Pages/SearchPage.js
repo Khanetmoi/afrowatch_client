926c5271b2f03fe2f85bd6271c688200e39d0278
Inayat-khan <khanetmoi@gmail.com>
2023-08-22 14:47:53 +0000

    search

--- afro/src/Pages/SearchPage.js
+++ afro/src/Pages/SearchPage.js
@@ -1,207 +1,56 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 import Slide from "./Slide";
-import comingTo from '../Images/comingTo.jpg';
 import styled from 'styled-components';
-
 
 const SearchedContent = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
-`
+`;
+
 const SearchPage = ({searchedMovie})=>{
+  const [content, setContent] = useState([]);
 
-    const handleCardClick = (sMovies) => {
-   
-        // setSelectedCard({...sMovies});
-      };
-    return (
-    //     <Slide
-    //     poster={baseUrlimage + slide.movie_image}
-    //     alter={slide.movie_name}
-    //     video={baseUrlTrailler + slide.movie_trailler_file}
-    //     title={slide.movie_name}
-    //     date={slide.movie_year_release}
-    //     className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-    //     key={index}
-    //     read={() => handleCardClick(slide)}
-    //     page={props.page}
-    //     log={props.logged}
-    //   />
- <SearchedContent>
-    <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
+  useEffect(() => {
+    // Fetch movie data from your API endpoint using fetch
+    fetch(`https://myworklm.com/Afrowatch_admin/api/movie/afrowatch_api_movie_search?search=${searchedMovie}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => {
+      setContent(data);
+    })
+    .catch(error => {
+      console.error("Error fetching movie data:", error);
+    });
+  }, [searchedMovie]);
 
-<Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-      <Slide
-        poster={comingTo}
-        alter={`hello`}
-        video='changer.mp4'
-        title='coming to america'
-        date='2021'
-        // className={`slide ${index === currentSlide ? 'slick-center' : ''}`}
-        key={1}
-        read={() => handleCardClick()}
-        // page={props.page}
-        // log={props.logged}
-      />
-  </SearchedContent>
-    // <h1>search</h1>
-    )
+  const handleCardClick = (sMovies) =>{
+    // setSelectedCard({...sMovies});
+  };
+
+  const baseUrlimage = "https://myworklm.com/Afrowatch_admin/server/movie_images/";
+  const baseUrlTrailler = "https://myworklm.com/Afrowatch_admin/server/trailer_files/";
+
+  return (
+    <SearchedContent>
+      {content.map(item => (
+        <Slide
+          key={item.movie_id}
+          poster={baseUrlimage + item.movie_image}
+          alter={item.movie_name}
+          video={baseUrlTrailler + item.movie_trailler_file}
+          title={item.movie_name}
+          date={item.movie_year_release}
+          read={() => handleCardClick(item)}
+        />
+      ))}
+    </SearchedContent>
+  )
 }
 
-export default SearchPage;
+export default SearchPage;
